4ba560c37120a134db79923ce9b7e13c0dd63224
hws-13151 <hws34810@gmail.com>
2024-09-27 17:30:53 +0900

    order footer

--- src/components/order/OrderFooter.jsx
+++ src/components/order/OrderFooter.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const OrderFooter = () => {
-  return <div>오더푸터입니다.</div>;
+  return (
+    <>
+      <div className="order-footer">
+        <div className="order-footer-con">
+          <div id='near'>NEARMARKET</div>
+          <br />
+          <div id='call'>0000 0000</div>
+          <br />
+          <div id='name'>주식회사 : 배고프조<br />
+          주문번호 : 0000-0000 고객센터 : 0000-0000 <br />
+          대표자 : 배고프조 주소 : 서울시 노원구 상계동</div>
+        </div>
+       </div>
+    </>
+  )
 };
 
 export default OrderFooter;
