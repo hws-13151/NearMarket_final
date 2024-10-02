30d1f2b4fa66b5751884a9d68e43dd04c7b44789
hws-13151 <hws34810@gmail.com>
2024-10-02 12:01:02 +0900

    admin css

--- src/layout/admin/AdminRight.jsx
+++ src/layout/admin/AdminRight.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AdminRight = () => {
   return (
-    <div>AdminRight</div>
+    <>
+      <div className="admin-right">
+        <div className="admin-right-con">
+          <div className="admin-right-header">
+            <ul>
+              <li>유저정보</li>
+              <li>LOGOUT</li>
+            </ul>
+          </div>
+          <Outlet />
+        </div>
+      </div>
+    </>
   )
 }
 
