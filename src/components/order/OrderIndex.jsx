5744651924d7185fc403540fce25896c4c3d32dd
hws-13151 <hws34810@gmail.com>
2024-10-08 13:05:00 +0900

    push

--- src/components/order/OrderIndex.jsx
+++ src/components/order/OrderIndex.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const OrderIndex = () => {
   const [indexList, setIndexList] = useState([]);
@@ -7,6 +8,7 @@ const OrderIndex = () => {
   const [vegetableItem, setVegetableItem] = useState({});
   const [snackItem, setSnackItem] = useState({});
   const [meatItem, setMeatItem] = useState({});
+  const navigate = useNavigate()
 
   useEffect (()=>{
     const fetchData = async () =>{
@@ -20,7 +22,7 @@ const OrderIndex = () => {
         ])
         setIndexList(indexRes.data)
         setFruitItem(fruitRes.data[0])
-        setVegetableItem(vegetableRes.data[0])
+        setVegetableItem(vegetableRes.data[3])
         setSnackItem(snackRes.data[3])
         setMeatItem(meatRes.data[0])
       }catch(error){
@@ -29,6 +31,27 @@ const OrderIndex = () => {
     }
     fetchData()
   }, [])
+  
+  const vegetableDetailFn = (e) => {
+    const eId = e.currentTarget.getAttribute('data-id')
+    navigate(`/order/vegetable/detail/${eId}`)
+  }
+  const meatDetailFn = (e) => {
+    const eId = e.currentTarget.getAttribute('data-id')
+    navigate(`/order/meat/meatDetail/${eId}`)
+  }
+  const fruitDetailFn = (e) => {
+    const eId = e.currentTarget.getAttribute('data-id')
+    navigate(`/order/fruit/detail/${eId}`)
+  }
+  const snackDetailFn = (e) => {
+    const eId = e.currentTarget.getAttribute('data-id')
+    navigate(`/order/snack/detail/${eId}`)
+  }
+  const indexDetailFn = (e) => {
+    const eId = e.currentTarget.getAttribute('data-id')
+    navigate(`/order/index/detail/${eId}`)
+  }
 
   return (
     <div className='order-index'>
@@ -36,44 +59,44 @@ const OrderIndex = () => {
         <div className='order-index-top'>
           <h2>코너별 추천상품</h2>
           <ul>
-            <li>
-              <div className="top">
-                <img src={`/images/fruit/${fruitItem.img}`} alt={fruitItem.title} />
-              </div>
-              <div className="bottom">
-                <h3>{fruitItem.title}</h3>
-                <p>{fruitItem.description}</p>
-                <span>{fruitItem.price ? fruitItem.price.toLocaleString() : '가격 정보 없음'}원</span>
-              </div>
-            </li>
-            <li>
+            <li data-id={vegetableItem.id} onClick={vegetableDetailFn}>
               <div className="top">
                 <img src={`/images/vegetable/${vegetableItem.img}`} alt={vegetableItem.title} />
               </div>
               <div className="bottom">
-                <h3>{vegetableItem.title}</h3>
+                <h4>{vegetableItem.title}</h4>
                 <p>{vegetableItem.description}</p>
                 <span>{vegetableItem.price ? vegetableItem.price.toLocaleString() : '가격 정보 없음'}원</span>
               </div>
             </li>
-            <li>
-              <div className="top">
-                <img src={`/images/ordersnack/${snackItem.img}`} alt={snackItem.title} />
-              </div>
-              <div className="bottom">
-                <h3>{snackItem.title}</h3>
-                <p>{snackItem.description}</p>
-                <span>{snackItem.price ? snackItem.price.toLocaleString() : '가격 정보 없음'}원</span>
-              </div>
-            </li>
-            <li>
+            <li data-id={meatItem.id} onClick={meatDetailFn}>
               <div className="top">
                 <img src={`/images/meat/${meatItem.img}`} alt={meatItem.title} />
               </div>
               <div className="bottom">
-                <h3>{meatItem.title}</h3>
+                <h4>{meatItem.title}</h4>
                 <p>{meatItem.description}</p>
                 <span>{meatItem.price ? meatItem.price.toLocaleString() : '가격 정보 없음'}원</span>
+              </div>
+            </li>
+            <li data-id={fruitItem.id} onClick={fruitDetailFn}>
+              <div className="top">
+                <img src={`/images/fruit/${fruitItem.img}`} alt={fruitItem.title} />
+              </div>
+              <div className="bottom">
+                <h4>{fruitItem.title}</h4>
+                <p>{fruitItem.description}</p>
+                <span>{fruitItem.price ? fruitItem.price.toLocaleString() : '가격 정보 없음'}원</span>
+              </div>
+            </li>
+            <li data-id={snackItem.id} onClick={snackDetailFn}>
+              <div className="top">
+                <img src={`/images/ordersnack/${snackItem.img}`} alt={snackItem.title} />
+              </div>
+              <div className="bottom">
+                <h4>{snackItem.title}</h4>
+                <p>{snackItem.description}</p>
+                <span>{snackItem.price ? snackItem.price.toLocaleString() : '가격 정보 없음'}원</span>
               </div>
             </li>
           </ul>
@@ -82,12 +105,12 @@ const OrderIndex = () => {
           <h2>이런 상품 어때요?</h2>
           <ul>
             {indexList && indexList.map((el, idx) => (
-              <li key={idx} data-id={el.id}>
+              <li key={idx} data-id={el.id} onClick={indexDetailFn}>
                 <div className="top">
                   <img src={`/images/orderIndex/${el.img}`} alt={el.img} />
                 </div>
                 <div className="bottom">
-                  <h3>{el.title}</h3>
+                  <h4>{el.title}</h4>
                   <p>{el.description}</p> 
                   <span>{el.price.toLocaleString()}원</span> 
                 </div>
---
+++ src/components/order/OrderIndexDetail.jsx
@@ -0,0 +1,142 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DetailModal from "./DetailModal";
+import { useDispatch } from "react-redux";
+import { addCart1 } from "../../slice/cartSlice1";
+
+const indexData = {
+  id: 0,
+  title: "",
+  price: 0,
+  img: "",
+  description: "",
+};
+
+const OrderIndexDetail = (param) => {
+  const navigate = useNavigate();
+  const [indexItem, setIndexItem] = useState(indexData);
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    const orderindexDetailFn = async () => {
+      const indexId = param.param.id;
+      try {
+        const res = await axios.get(
+          `http://localhost:3001/indexItems?id=${indexId}`
+        );
+        setIndexItem(res.data[0]);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    orderindexDetailFn();
+  }, []);
+
+  const IncrementFn = () => {
+    setCount(count + 1);
+  };
+
+  const DecrementFn = () => {
+    if (count === 1) {
+      setCount(1);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  const [isModal, setIsModal] = useState(false);
+
+  const onModalFn = (e) => {
+    setIsModal(true);
+  };
+
+  const dispatch = useDispatch();
+
+  const addCartFn2 = () => {
+    const setItemCart = {
+      id: indexItem.id,
+      title: indexItem.title,
+      price: indexItem.price,
+      img: `/images/index/${indexItem.img}`,
+      count: count,
+      category: "indexItems",
+    };
+    dispatch(addCart1(setItemCart));
+  };
+
+  return (
+    <>
+      {isModal && <DetailModal setIsModal={setIsModal} />}
+      <div className="order-index-detail">
+        <div className="order-index-detail-con">
+          <div className="left">
+            <div className="left-img">
+              <img src={`/images/orderindex/${indexItem.img}`} alt={indexItem.img} />
+            </div>
+          </div>
+          <div className="right">
+            <div className="right-top">
+              <ul>
+                <li>NM.K</li>
+                <li>
+                  <h1>{indexItem.title}</h1>
+                </li>
+                <li>{indexItem.description}</li>
+                <li>
+                  <h1>{indexItem.price.toLocaleString()}원</h1>
+                </li>
+                <li>
+                  <h3>상품설명 :</h3>
+                  <br />
+                  <p>
+                    
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="right-bottom">
+              <div className="bottom">
+                <div className="bottom-price">
+                  <ul>
+                    <li>{indexItem.title}</li>
+                    <li>
+                      <button onClick={IncrementFn}>+</button>
+                      <span>{count}</span>
+                      <button onClick={DecrementFn}>-</button>
+                      <p>{(indexItem.price * count).toLocaleString()}원</p>
+                    </li>
+                  </ul>
+                </div>
+                <div className="bottom-payment">
+                  <ul>
+                    <li>
+                      <p>합계</p>
+                      <h1>{(indexItem.price * count).toLocaleString()}원</h1>
+                    </li>
+                    <hr />
+                    <li>
+                      <button
+                        onClick={() => {
+                          navigate(-1);
+                        }}
+                      >
+                        이전페이지
+                      </button>
+                      <button onClick={addCartFn2} onClickCapture={onModalFn}>
+                        장바구니
+                      </button>
+                      <button>결제</button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrderIndexDetail;
