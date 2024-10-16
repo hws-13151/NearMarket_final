6c29dabb4d649ba9eaa998668b86c8e8fa4cc0b8
“hws-13151” <hws34810@gmail.com>
2024-10-16 12:01:45 +0900

    쌈뽕한 반응형

--- src/components/admin/AdminPayment.jsx
+++ src/components/admin/AdminPayment.jsx
@@ -36,49 +36,53 @@ const AdminPayment = () => {
     <>
       <div className="admin-payment">
         <div className="admin-payment-con">
-          <h1>회원 결제내역</h1>
-          <span style={{ display: 'block', margin: '20px 0' }}><SearchBox handleChange={handleChange} /></span>
-          <table>
-            <thead>
-              <tr>
-                <th>회원아이디</th>
-                <th>주문일자</th>
-                <th>상품상세내역</th>
-                <th>주문금액</th>
-                <th>주문처</th>
-                <th>주문방식</th>
-                <th>주소</th>
-                <th>배송메시지</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredPaymentMember && filteredPaymentMember.map((el, idx) => (
-                <tr key={idx}>
-                  <td>{el.memberEmail}</td>
-                  <td>{el.time}</td>
-                  <td>
-                    {/* el.paymentResult가 객체일 경우 */}
-                    {Array.isArray(el.paymentResult) ? (
-                      el.paymentResult.map((item, index) => (
-                        <div key={index}>
-                          {/* item 객체의 개별 속성 렌더링 */}
-                          {item.title} : {item.count}개
-                        </div>
-                      ))
-                    ) : (
-                      "상품 정보가 없습니다."
-                    )}
-                  </td>
-                  <td>{el.paymentAmount.toLocaleString()}원</td>
-                  <td>{el.shopVal}</td>
-                  <td>{el.orderMethod}</td>
-                  <td>{el.orderAddress}</td>
-                  <td>{el.addressMessage}</td>
+            <h1>회원 결제내역</h1>
+          <div className="admin-payment-header">
+            <span style={{ display: 'block'}}><SearchBox handleChange={handleChange} /></span>
+          </div>
+          <div className="admin-payment-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>회원아이디</th>
+                  <th>주문일자</th>
+                  <th>상품상세내역</th>
+                  <th>주문금액</th>
+                  <th>주문처</th>
+                  <th>주문방식</th>
+                  <th>주소</th>
+                  <th>배송메시지</th>
                 </tr>
-              ))}
+              </thead>
+              <tbody>
+                {filteredPaymentMember && filteredPaymentMember.map((el, idx) => (
+                  <tr key={idx}>
+                    <td>{el.memberEmail}</td>
+                    <td>{el.time}</td>
+                    <td>
+                      {/* el.paymentResult가 객체일 경우 */}
+                      {Array.isArray(el.paymentResult) ? (
+                        el.paymentResult.map((item, index) => (
+                          <div key={index}>
+                            {/* item 객체의 개별 속성 렌더링 */}
+                            {item.title} : {item.count}개
+                          </div>
+                        ))
+                      ) : (
+                        "상품 정보가 없습니다."
+                      )}
+                    </td>
+                    <td>{el.paymentAmount.toLocaleString()}원</td>
+                    <td>{el.shopVal}</td>
+                    <td>{el.orderMethod}</td>
+                    <td>{el.orderAddress}</td>
+                    <td>{el.addressMessage}</td>
+                  </tr>
+                ))}
 
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
