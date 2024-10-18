d0b8abe5a74b2d4b41e9121fdca93107fc0ab635
“hws-13151” <hws34810@gmail.com>
2024-10-18 16:53:43 +0900

    commit

--- src/components/admin/ApiShopModal.jsx
+++ src/components/admin/ApiShopModal.jsx
@@ -68,7 +68,7 @@ const ApiShopModal = ({ shop, onClose }) => {
             <div className="paymentApi-modal-con">
                 <button className="paymentApi-close-btn" onClick={onClose}>X</button>
                 <h3>{shop.title}의 위치</h3>
-                <div id="shop-map" style={{ width: 'calc(500px - 40px)', height: '500px', boxSizing: 'border-box' }}></div>
+                <div id="shop-map" style={{ width: '100%', height: '500px', boxSizing: 'border-box' }}></div>
             </div>
         </div>
     );
