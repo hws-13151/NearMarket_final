821a24900214875c4bee65897fb99bd877f16af1
“hws-13151” <hws34810@gmail.com>
2024-10-18 14:19:28 +0900

    push

--- src/components/admin/AdminIndex.jsx
+++ src/components/admin/AdminIndex.jsx
@@ -11,13 +11,13 @@ const AdminIndex = () => {
         <div className="admin-index-con">
           <ul>
             <li onClick={()=>{navigate('/admin/members')}}><p>회원정보</p></li>
-            <li onClick={()=>{navigate('/admin/best')}}><p>베스트코너 아이템</p></li>
-            <li onClick={()=>{navigate('/admin/vegetable')}}><p>채소코너 아이템</p></li>
-            <li onClick={()=>{navigate('/admin/meat')}}><p>고기코너 아이템</p></li>
-            <li onClick={()=>{navigate('/admin/fruit')}}><p>과일코너 아이템</p></li>
-            <li onClick={()=>{navigate('/admin/snack')}}><p>과자코너 아이템</p></li>
+            <li onClick={()=>{navigate('/admin/best')}}><p>베스트코너</p></li>
+            <li onClick={()=>{navigate('/admin/vegetable')}}><p>채소코너</p></li>
+            <li onClick={()=>{navigate('/admin/meat')}}><p>고기코너</p></li>
+            <li onClick={()=>{navigate('/admin/fruit')}}><p>과일코너</p></li>
+            <li onClick={()=>{navigate('/admin/snack')}}><p>과자코너</p></li>
             <li onClick={()=>{navigate('/admin/cart')}}><p>장바구니</p></li>
-            <li onClick={()=>{navigate('/admin/produtcsInsert')}}><p>상품등록 바로가기</p></li>
+            <li onClick={()=>{navigate('/admin/produtcsInsert')}}><p>상품등록</p></li>
             <li onClick={()=>{navigate('/admin/shop')}}><p>주문처 관리</p></li>
             <li onClick={()=>{navigate('/admin/payment')}}><p>결제 관리</p></li>
           </ul>
